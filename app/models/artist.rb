fb16e2708584f462651dbe047c9d1ab303e5d8bb
DavidNNussbaum <brocknynj@aol.com>
2021-03-14 13:46:53 -0400

    commit tree 408baf01ba56d4b3c712d7aec6d0e9aae8be29cc

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,15 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def index
+        @artists = Artist.all
+      end
+ 
+     def show
+       @artist = Artist.find(params[:id])
+     end
+ 
+    def song_count
+      self.songs.size
+    end
 end
