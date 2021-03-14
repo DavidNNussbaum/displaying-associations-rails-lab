cb1db9e8798152390302791335ac6253329f2dfb
DavidNNussbaum <brocknynj@aol.com>
2021-03-14 14:02:42 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,16 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def index
+    @songs = Song.all
+  end
+
+ def show
+   @song = Song.find(params[:id])
+ end
+
+ def artist_name
+  self.artist.name
+end
+
 end
---
+++ db/migrate/20210312215849_add_foreign_key_artists.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyArtists < ActiveRecord::Migration[5.0]
+  def change
+    add_column :artists, :song_id, :integer
+  end
+end
